7fac72a3a8cc6e036a1a56b7a3f89728ea02edc0
manbago <manbago@gmail.com>
2021-11-28 11:17:21 +0100

    Infinite Scroll

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -5,41 +5,53 @@ import { get } from "../utils/httpClient";
 import { MovieCard } from "./MovieCard";
 import styles from "./MoviesGrid.module.css";
 import { Spinner } from "./Spinner";
+import InfinityScroll from "react-infinite-scroll-component";
 
 
 
-export function MoviesGrid() {
+export function MoviesGrid({ search }) {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [hasmore, setHasMore] = useState(true);
 
-  const query = useQuery();
-  const search = query.get("search");
+  // const query = useQuery();
+  // const search = query.get("search");
+
   // console.log(search);
-
   // const location = useLocation();
   // console.log(location.search);
 
   useEffect(() => {
     setIsLoading(true);
     const searchUrl = search
-     ? "/search/movie?query=" + search 
-     : "/discover/movie";
+     ? "/search/movie?query=" + search + "&page=" + page
+     : "/discover/movie?page=" + page;
     get(searchUrl).then((data) => {
-      setMovies(data.results);
+      setMovies((prevMovies) => prevMovies.concat(data.results));
+      setHasMore(data.page < data.total_pages);
+      // setMovies(data.results);
       setIsLoading(false);
     });
-  }, [search]);
+  }, [search, page]);
   // }, []);
 
-if (isLoading) {
-    return <Spinner />;
-  }
+// if (isLoading) {
+//     return <Spinner />;
+//   }
 
   return (
-    <ul className={styles.moviesGrid}>
+    <InfinityScroll 
+      dataLength={movies.length} 
+      hasMore={true} 
+      next={() => setPage((prevPago) => prevPago +1 )}
+      loader={<Spinner />}
+    >
+      <ul className={styles.moviesGrid}>
       {movies.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
-    </ul>
+      </ul>
+    </InfinityScroll>
   );
 }
